0e5b33b934e8d12e2d255914188958a3e83ba214
Pushpendra-Singh27 <shivasingh.tg27@gmail.com>
2025-08-29 21:58:34 +0530

    server2

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  base: '/', // Set this to your subdirectory if deploying to a subdirectory, e.g., '/my-app/'
+export default defineConfig({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    sourcemap: false
   },
   plugins: [react()],
   resolve: {
@@ -21,7 +15,21 @@ export default defineConfig(({ command }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
   define: {
     'process.env': {}
   }
-}));
+});
